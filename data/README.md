ca9f1924d6def479280a7992ba18adf0d343e470
Martin Filliau <martin@filliau.com>
2014-03-27 15:25:46 +0000

    Migrated data file

---
+++ data/README.md
@@ -0,0 +1,33 @@
+Maps.ox data
+============
+
+All university data
+-------------------
+
+We are getting a GeoJSON feed from Mobile Oxford, the URL is as follow:
+
+    http://127.0.0.1:5000/places/search.geojson?type_exact=/university/college&type_exact=/university/hall&type_exact=/university/department&type_exact=/university/building&type_exact=/university/library&type_exact=/university/site&type_exact=/leisure/museum&count=5000
+
+Curated sites
+-------------
+
+Curated sites should as much as possible come from OxPoints data / the GeoJSON feed.
+
+However, it is possible to create arbitrary shapes and/or points potentially using online tools such as http://geojson.io/.
+
+One property is mandatory for each feature: `short_name`.
+
+GeoJSON properties
+------------------
+
+Each feature should have the following properties:
+
+* `type`
+* `type_name`
+* `name`
+* `short_name`
+
+University shapes
+-----------------
+
+The script situated in `data/simplify-university-shapes.py` should be executed against the main GeoJSON feed to reduce all shapes to a set of "unique" shapes per type to be used to produce the actual shapes in the tiles.
---
+++ data/simplify-university-shapes.py
@@ -0,0 +1,36 @@
+import json
+from geojson import load as geojson_load
+from geojson import dump as geojson_dump
+from geojson import Feature, FeatureCollection
+
+def simplify(features):
+    shapes = dict()
+    for feature in features['features']:
+        coord = feature['geometry']['coordinates']
+        feat_type = feature['properties']['type_name']
+        key = '{type_name}-{coords}'.format(type_name=feat_type,
+                                            coords=str(coord))
+        shapes[key] = feature
+    return shapes
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='Makes sure there is only one shape per type per coordinates in the produced GeoJSON feed')
+    parser.add_argument('shapes', type=argparse.FileType('r'), help='File containing all shapes, expected to be reduced')
+    parser.add_argument('output', type=argparse.FileType('w'), help='File to be written with only one shape per type per coordinates')
+    parser.add_argument('--indent', dest='indent', action='store_true', default=False, help='Indent the JSON file to be written')
+    parser.add_argument('--verbose', dest='verbose', action='store_true', default=False, help='Provides some statistics on reduced shapes')
+    ns = parser.parse_args()
+    features = json.load(ns.shapes)
+    if ns.verbose:
+        print "{count} features in original file".format(count=len(features['features']))
+    shapes = simplify(features)
+    if ns.verbose:
+        print "{count} features in new file".format(count=len(shapes))
+    features = FeatureCollection(shapes.values())
+    geojson_dump(features, ns.output, indent=ns.indent)
+
+                                    
+if __name__ == '__main__':
+    main()
