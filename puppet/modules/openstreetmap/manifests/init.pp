5be259249c74351b62825f8e5a8f6f1e2cd44a18
Martin Filliau <martin@filliau.com>
2014-03-28 13:50:54 +0000

    FIxed encoding problem of postgresql

--- puppet/modules/openstreetmap/manifests/init.pp
+++ puppet/modules/openstreetmap/manifests/init.pp
@@ -1,18 +1,33 @@
 class openstreetmap {
 
-    class { 'postgresql::server': }
-
+    class { 'postgresql::globals' :
+      encoding => 'UTF8',
+      locale   => 'en_GB.UTF-8',
+    }->
+    class { 'postgresql::server' :
+        encoding => 'UTF8'
+    }->
+    # workaround for http://projects.puppetlabs.com/issues/4695
+    # when PostgreSQL is installed with SQL_ASCII encoding instead of UTF8
+    exec { 'utf8 postgres' :
+      command => 'pg_dropcluster --stop 9.1 main ; pg_createcluster --start --locale en_GB.UTF-8 9.1 main',
+      unless  => 'sudo -u postgres psql -t -c "\l" | grep template1 | grep -q UTF',
+      require => Class['postgresql::server'],
+      path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
+    }
+    
     include ::postgis
     
     $db_user = 'tilemill'
     
     postgresql::server::role { $db_user :
           password_hash => false,
+          require => Exec['utf8 postgres']
     }
     
     postgis::database { 'osm' :
         owner => $db_user,
-        charset => 'SQL_ASCII',
+        charset => 'UTF8',
         require => Postgresql::Server::Role[$db_user]
     }
     
