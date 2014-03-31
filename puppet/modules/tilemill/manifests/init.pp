721ed155e850ec1ec441fe9b673b35099c71bfbd
Martin Filliau <martin@filliau.com>
2014-03-31 16:57:04 +0100

    Git shared repo

--- puppet/modules/tilemill/manifests/init.pp
+++ puppet/modules/tilemill/manifests/init.pp
@@ -3,6 +3,7 @@ class tilemill {
 
     $user = 'tilemill'
     $user_directory = "/srv/${user}"
+    $maps_project_dir = "${user_directory}/maps-tiles"
     $git_repo = 'https://github.com/ox-it/maps-tiles.git'
     $mapbox_project = '/usr/share/mapbox/project/maps-ox'
 
@@ -54,14 +55,29 @@ class tilemill {
     }
     
     exec { "git-clone-project" :
-        command => "git clone ${git_repo} ${user_directory}/maps-tiles",
+        command => "git clone ${git_repo} ${maps_project_dir}",
         user => 'tilemill',
-        unless => "test -d ${user_directory}/maps-tiles",
+        unless => "test -d ${maps_project_dir}",
         require => [File[$user_directory], Package['git']]
     }
     
+    # make sure the git repo can be shared between different users
+    exec { "git-share-repo" :
+        command => "git config core.sharedRepository true",
+        user => 'tilemill',
+        cwd => $maps_project_dir,
+        require => Exec["git-clone-project"]
+    }
+    
+    file {$maps_project_dir :
+        group => 'tilemill',
+        mode => 'g+swX',
+        recurse => true,
+        require => Exec['git-clone-project'],
+    }
+    
     exec { "copy-to-mapbox" :
-        command => "cp -R ${user_directory}/maps-tiles/maps-ox/ /usr/share/mapbox/project/maps-ox",
+        command => "cp -R ${maps_project_dir}/maps-ox/ /usr/share/mapbox/project/maps-ox",
         user => 'mapbox',
         unless => "test -d /usr/share/mapbox/project/maps-ox",
         require => [Service['tilemill'], Exec['git-clone-project']]
