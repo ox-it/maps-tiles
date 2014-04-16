b1b15f19a1533e299a14c155869b46364161d327
Martin Filliau <martin@filliau.com>
2014-04-16 16:14:41 +0100

    Generate all outputs

--- data/build-data.py
+++ data/build-data.py
@@ -20,58 +20,81 @@ def _get_feature(ident, full_name, short_name, type_name, geometry):
                                 'short_name': short_name or full_name,
                                 'type_name': type_name})
 
-def _get_type(graph, oxp_type, type_name):
+def _get_type(graph, oxp_type, type_name, ignore=None):
     """Get all things from a type from OxPoints, use shape or fallback on lat/lon
     :param graph: rdflib Graph
+    :param oxp_type: type expected
+    :param type_name: mapped (friendly) type name
+    :param ignore: list of subjects to ignore
     :return list
     """
-    features = []
+    ignore = ignore or list()
+    processed = list()
+    features = list()
     for subject in graph.subjects(RDF.type, oxp_type):
-        title = graph.value(subject, DC.title)
-        short_name = graph.value(subject, OxPoints.shortLabel)
-        shape = graph.value(subject, Geometry.extent)
-        if shape:
-            wkt = graph.value(shape, Geometry.asWKT).toPython()
-            features.append(_get_feature(subject, title, short_name, type_name, wkt_loads(wkt)))
-        # fallback on latitude / longitude
-        elif (subject, Geo.lat, None) in graph and (subject, Geo.long, None) in graph:
-            lat = graph.value(subject, Geo.lat).toPython()
-            lon = graph.value(subject, Geo.long).toPython()
-            features.append(_get_feature(subject, title, short_name, type_name, Point(float(lon), float(lat))))
-        else:
-            primary_place = graph.value(subject, OxPoints.primaryPlace)
-            if primary_place:
-                shape = graph.value(primary_place, Geometry.extent)
-                if shape:
-                    wkt = graph.value(shape, Geometry.asWKT).toPython()
-                    features.append(_get_feature(subject, title, short_name, type_name, wkt_loads(wkt)))
-                elif (primary_place, Geo.lat, None) in graph and (primary_place, Geo.long, None) in graph:
-                    lat = graph.value(primary_place, Geo.lat).toPython()
-                    lon = graph.value(primary_place, Geo.long).toPython()
-                    features.append(_get_feature(subject, title, short_name, type_name, Point(float(lon), float(lat))))
-
-
-    return features
+        if subject not in ignore:
+            title = graph.value(subject, DC.title)
+            short_name = graph.value(subject, OxPoints.shortLabel)
+            shape = graph.value(subject, Geometry.extent)
+            if shape:
+                wkt = graph.value(shape, Geometry.asWKT).toPython()
+                features.append(_get_feature(subject, title, short_name, type_name, wkt_loads(wkt)))
+                processed.append(subject)
+            # fallback on latitude / longitude
+            elif (subject, Geo.lat, None) in graph and (subject, Geo.long, None) in graph:
+                lat = graph.value(subject, Geo.lat).toPython()
+                lon = graph.value(subject, Geo.long).toPython()
+                features.append(_get_feature(subject, title, short_name, type_name, Point(float(lon), float(lat))))
+                processed.append(subject)
+            else:
+                primary_place = graph.value(subject, OxPoints.primaryPlace)
+                if primary_place:
+                    shape = graph.value(primary_place, Geometry.extent)
+                    if shape:
+                        wkt = graph.value(shape, Geometry.asWKT).toPython()
+                        features.append(_get_feature(subject, title, short_name, type_name, wkt_loads(wkt)))
+                        processed.append(subject)
+                        processed.append(primary_place)
+                    elif (primary_place, Geo.lat, None) in graph and (primary_place, Geo.long, None) in graph:
+                        lat = graph.value(primary_place, Geo.lat).toPython()
+                        lon = graph.value(primary_place, Geo.long).toPython()
+                        features.append(_get_feature(subject, title, short_name, type_name, Point(float(lon), float(lat))))
+                        processed.append(subject)
+                        processed.append(primary_place)
+    return features, processed
 
 
 def do_buildings(graph):
-    features = _get_type(graph, OxPoints.Building, 'Building')
+    features, processed = _get_type(graph, OxPoints.Building, 'Building')
     return FeatureCollection(features)
 
 def do_colleges(graph):
-    features = []
-    features.extend(_get_type(graph, OxPoints.College, 'College'))
-    features.extend(_get_type(graph, OxPoints.Hall, 'Hall'))
-    features.extend(_get_type(graph, OxPoints.Site, 'Site'))
+    types = [
+        (OxPoints.College, 'College'),
+        (OxPoints.Hall, 'Hall'),
+    ]
+    features = list()
+    to_ignore = list()
+    for oxp_type, type_name in types:
+        feats, processed = _get_type(graph, oxp_type, type_name)
+        features.extend(feats)
+        to_ignore.extend(processed)
+    feats, processed = _get_type(graph, OxPoints.Site, 'Site', ignore=to_ignore)
+    features.extend(feats)
     return FeatureCollection(features)
 
 def do_departments(graph):
+    types = [
+        (OxPoints.Department, 'Department'),
+        (OxPoints.Faculty, 'Department'),
+        (OxPoints.Unit, 'Department'),
+        (OxPoints.Library, 'Library'),
+        (OxPoints.Museum, 'Museum')
+    ]
     features = list()
-    features.extend(_get_type(graph, OxPoints.Department, 'Department'))
-    features.extend(_get_type(graph, OxPoints.Faculty, 'Department'))
-    features.extend(_get_type(graph, OxPoints.Unit, 'Department'))
-    features.extend(_get_type(graph, OxPoints.Library, 'Library'))
-    features.extend(_get_type(graph, OxPoints.Museum, 'Museum'))
+    for oxp_type, type_name in types:
+        feats, processed = _get_type(graph, oxp_type, type_name)
+        features.extend(feats)
 
     shapes = dict()
     for feature in features:
