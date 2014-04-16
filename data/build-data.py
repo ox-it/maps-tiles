51d1e8e4f5d14d0a0475949d73e57747713ced5c
Martin Filliau <martin@filliau.com>
2014-04-16 14:11:37 +0100

    Working buildings

--- data/build-data.py
+++ data/build-data.py
@@ -1,58 +1,101 @@
+import sys
+
 import rdflib
+from rdflib import RDF
+from rdflib.namespace import Namespace, DC
 from geojson import load as geojson_load
-from geojson import dump as geojson_dump
+from geojson import dumps as geojson_dumps
 from geojson import Feature, FeatureCollection
+from shapely.wkt import loads as wkt_loads
+from shapely.geometry import Point
 
-def simplify(features, differentiate_types):
-    """Simplify a GeoJSON feed to avoid exact forms duplication
-    :param features: dict of a GeoJSON FeatureCollection
-    :param differentiate_types: bool decide if we differentiate per type or not
-    :return dict simplified shapes per coordinates and per type depending on differentiate_types
+OxPoints = Namespace('http://ns.ox.ac.uk/namespace/oxpoints/2009/02/owl#')
+Geometry = Namespace('http://data.ordnancesurvey.co.uk/ontology/geometry/')
+Geo = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
+
+def _get_feature(ident, name, type_name, geometry):
+    oxpoints_id = 'oxpoints:{ident}'.format(ident=ident.toPython().rsplit('/')[-1])
+    return Feature(id=oxpoints_id, geometry=geometry,
+                    properties={'title': name,
+                                'type': type_name})
+
+
+def do_buildings(graph):
+    """Get all buildings from OxPoints, use shape or fallback on lat/lon
+    :param graph: rdflib Graph
+    :return FeatureCollection
     """
-    shapes = dict()
-    for feature in features['features']:
-        coord = feature['geometry']['coordinates']
-        type_name = feature['properties']['type_name']
-        if differentiate_types:
-          key = '{coords}-{type}'.format(coords=str(coord),
-                                          type=type_name)
-        else:
-          key = '{coords}'.format(coords=str(coord))
-        shapes[key] = feature
-    return shapes
+    features = []
+    for subject in graph.subjects(RDF.type, OxPoints.Building):
+        title = graph.value(subject, DC.title)
+        type_name = 'Building'
+        shape = graph.value(subject, Geometry.extent)
+        if shape:
+            wkt = graph.value(shape, Geometry.asWKT).toPython()
+            features.append(_get_feature(subject, title, type_name, wkt_loads(wkt)))
+        # fallback on latitude / longitude
+        elif (subject, Geo.lat, None) in graph and (subject, Geo.long, None) in graph:
+            lat = graph.value(subject, Geo.lat).toPython()
+            lon = graph.value(subject, Geo.long).toPython()
+            features.append(_get_feature(subject, title, type_name, Point(float(lon), float(lat))))
 
-def do_buildings():
+    return FeatureCollection(features)
+
+def do_colleges(graph):
   pass
 
-def do_colleges():
-  pass
-
-def do_departments():
+def do_departments(graph):
   pass
 
 
 FUNCTIONS = {
-  'buildings': do_buildings,
+  'buildings': do_buildings,    # Buildings only
   'colleges': do_colleges,   # Site, College, Hall
   'departments': do_departments,  # Department, Library, Museum
 }
+
+DEFAULT_SERIALIZATION = 'text/turtle'
+
+MAPPED_TYPES = [
+    (OxPoints.University, 'University'),
+    (OxPoints.College, 'College'),
+    (OxPoints.Department, 'Department'),
+    (OxPoints.Faculty, 'Department'),
+    (OxPoints.Unit, 'Department'),
+    (OxPoints.Library, 'Library'),
+    (OxPoints.SubLibrary, 'SubLibrary'),
+    (OxPoints.Division, 'Division'),
+    (OxPoints.Museum, 'Museum'),
+    (OxPoints.CarPark, 'CarPark'),
+    (OxPoints.Room, 'Room'),
+    (OxPoints.Hall, 'Hall'),
+    (OxPoints.Building, 'Building'),
+    (OxPoints.Space, 'Space'),
+    (OxPoints.Site, 'Site')
+]
 
 
 def main():
     import argparse
     parser = argparse.ArgumentParser(description='Produce GeoJSON feeds to be used by TileMill')
-    parser.add_argument('oxpoints_file', type=argparse.FileType('r'), help='Main RDF dump expected in text/turtle format')
+    parser.add_argument('oxpoints_file', type=argparse.FileType('r'), help='Main RDF dump expected in {default_serialization} format'.format(default_serialization=DEFAULT_SERIALIZATION))
     parser.add_argument('oxpoints_shape', type=argparse.FileType('r'), help='Shapes RDF dump expected in text/turtle format')
-    parser.add_argument('function', help='Function: either buildings, colleges or departments')
-    parser.add_argument('--rdf-serialization', dest='rdf_serialization', action='store_true', default='text/turtle', help='RDF serialization, defaults to text/turtle')
+    parser.add_argument('function', help="Function (should be one of '{available_functions}')".format(available_functions=', '.join(FUNCTIONS.iterkeys())))
+    parser.add_argument('--rdf-serialization', dest='rdf_serialization', action='store_true', default=DEFAULT_SERIALIZATION, help='RDF serialization, defaults to text/turtle')
 
     ns = parser.parse_args()
 
-    print("==> Loading data into the graph")
+    sys.stderr.write("==> Loading data into the graph\n")
     graph = rdflib.Graph()
     graph.parse(ns.oxpoints_file, format=ns.rdf_serialization)
     graph.parse(ns.oxpoints_shape, format=ns.rdf_serialization)
-    print("==> Data loaded")
+    sys.stderr.write("==> Data loaded\n")
+
+    if ns.function not in FUNCTIONS:
+        sys.exit("Incorrect function (should be one of '{available_functions}')".format(available_functions=', '.join(FUNCTIONS.iterkeys())))
+    else:
+        collection = FUNCTIONS[ns.function](graph)
+        sys.stdout.write(geojson_dumps(collection))
 
 
 if __name__ == '__main__':
