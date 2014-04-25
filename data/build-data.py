b5b69a65c97e4213437e7fa6af572cfe3e3e86c6
Martin Filliau <martin@filliau.com>
2014-04-25 11:22:35 +0100

    Updated script to use map label as a first choice

--- data/build-data.py
+++ data/build-data.py
@@ -13,19 +13,17 @@ OxPoints = Namespace('http://ns.ox.ac.uk/namespace/oxpoints/2009/02/owl#')
 Geometry = Namespace('http://data.ordnancesurvey.co.uk/ontology/geometry/')
 Geo = Namespace('http://www.w3.org/2003/01/geo/wgs84_pos#')
 
-def _get_feature(ident, full_name, short_name, type_name, geometry):
+def _get_feature(ident, name, type_name, geometry):
     """Prepare a GeoJSON feature
     :param ident: URI of the Thing
-    :param full_name: long version of the name
-    :param short_name: short version of the name (will fallback on full_name)
+    :param name: feature name
     :param type_name: name of the type
     :param geometry: geometry (shapely)
     :return geojson.Feature
     """
     oxpoints_id = 'oxpoints:{ident}'.format(ident=ident.toPython().rsplit('/')[-1])
     return Feature(id=oxpoints_id, geometry=geometry,
-                    properties={'name': full_name,
-                                'short_name': short_name or full_name,
+                    properties={'name': name,
                                 'type_name': type_name})
 
 def _get_type(graph, oxp_type, type_name, ignore=None):
@@ -43,16 +41,19 @@ def _get_type(graph, oxp_type, type_name, ignore=None):
         if subject not in ignore:
             title = graph.value(subject, DC.title)
             short_name = graph.value(subject, OxPoints.shortLabel)
+            map_label = graph.value(subject, OxPoints.mapLabel)
+            # name is the map label defined but fallbacks on short name or title
+            name = map_label or short_name or title
             shape = graph.value(subject, Geometry.extent)
             if shape:
                 wkt = graph.value(shape, Geometry.asWKT).toPython()
-                features.append(_get_feature(subject, title, short_name, type_name, wkt_loads(wkt)))
+                features.append(_get_feature(subject, name, type_name, wkt_loads(wkt)))
                 processed.append(subject)
             # fallback on latitude / longitude
             elif (subject, Geo.lat, None) in graph and (subject, Geo.long, None) in graph:
                 lat = graph.value(subject, Geo.lat).toPython()
                 lon = graph.value(subject, Geo.long).toPython()
-                features.append(_get_feature(subject, title, short_name, type_name, Point(float(lon), float(lat))))
+                features.append(_get_feature(subject, name, type_name, Point(float(lon), float(lat))))
                 processed.append(subject)
             # try to get info from the primary place of the thing
             else:
@@ -61,13 +62,13 @@ def _get_type(graph, oxp_type, type_name, ignore=None):
                     shape = graph.value(primary_place, Geometry.extent)
                     if shape:
                         wkt = graph.value(shape, Geometry.asWKT).toPython()
-                        features.append(_get_feature(subject, title, short_name, type_name, wkt_loads(wkt)))
+                        features.append(_get_feature(subject, name, type_name, wkt_loads(wkt)))
                         processed.append(subject)
                         processed.append(primary_place)
                     elif (primary_place, Geo.lat, None) in graph and (primary_place, Geo.long, None) in graph:
                         lat = graph.value(primary_place, Geo.lat).toPython()
                         lon = graph.value(primary_place, Geo.long).toPython()
-                        features.append(_get_feature(subject, title, short_name, type_name, Point(float(lon), float(lat))))
+                        features.append(_get_feature(subject, name, type_name, Point(float(lon), float(lat))))
                         processed.append(subject)
                         processed.append(primary_place)
     return features, processed
